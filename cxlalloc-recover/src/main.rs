05064ce4f919532bac5790ac6aebcf8ec0df582d
Newton Ni <nwtnni@gmail.com>
2024-10-17 00:30:55 -0500

    Finish recovery harness
    Add support for blocking vs. non-blocking recovery.

--- cxlalloc-recover/src/main.rs
+++ cxlalloc-recover/src/main.rs
@@ -1,7 +1,10 @@
 use core::cell::Cell;
+use core::sync::atomic::AtomicBool;
 use core::sync::atomic::AtomicU64;
+use core::sync::atomic::AtomicUsize;
 use core::sync::atomic::Ordering;
 use std::sync::Barrier;
+use std::sync::Mutex;
 use std::sync::OnceLock;
 
 use clap::Parser;
@@ -11,26 +14,34 @@ use memento::ds::queue::Queue;
 
 use memento::ploc::Checkpoint;
 use memento::ploc::Handle;
+use memento::pmem::PAllocator as _;
+use memento::pmem::PMEMAllocator;
 use memento::pmem::{Collectable, GarbageCollection, Pool, PoolHandle, RootObj};
 use memento::{Collectable, Memento};
 
 #[derive(Parser)]
 struct Cli {
-    /// Crash the process at this iteration
+    /// Crash this thread
     #[arg(long)]
-    process: Option<u64>,
+    thread: Option<usize>,
 
-    /// Crash a single thread at these iterations
+    /// Crash at these iterations
     #[arg(long, use_value_delimiter = true, value_delimiter = ',')]
-    thread: Vec<u64>,
+    crash: Vec<u64>,
+
+    /// Block for garbage collection
+    #[arg(long)]
+    block: bool,
 
     /// File name
     #[arg(long, default_value = "/dev/shm/pool")]
     path: String,
 
-    /// Create or reopen
+    #[arg(long, default_value_t = 1_000_000)]
+    objects: u64,
+
     #[arg(long)]
-    create: bool,
+    threads: Option<u64>,
 
     /// Heap size
     #[arg(long, default_value_t = 1 << 32)]
@@ -38,22 +49,36 @@ struct Cli {
 }
 
 fn main() {
-    let cli = Cli::parse();
+    let mut cli = Cli::parse();
 
-    if let Some(process) = cli.process {
-        CRASH_PROCESS.store(process, Ordering::Relaxed);
-    } else if !cli.thread.is_empty() {
-        CRASH_THREAD.get_or_init(|| cli.thread.clone());
+    if let Some(thread) = cli.thread {
+        CRASH_THREAD.store(thread, Ordering::Relaxed);
+    }
+    if !cli.crash.is_empty() {
+        cli.crash.sort_unstable();
+        cli.crash.reverse();
+        *CRASH.lock().unwrap() = cli.crash.clone();
     }
 
-    let pool = if cli.create {
-        Pool::create::<Queue<u64>, Mmt>(&cli.path, cli.size, THREADS as usize).unwrap()
-    } else {
-        unsafe { Pool::open::<Queue<u64>, Mmt>(&cli.path, cli.size).unwrap() }
-    };
+    let threads = cli.threads.unwrap_or_else(|| {
+        std::thread::available_parallelism()
+            .expect("Call to available_parallelism failed, pass thread count explicitly")
+            .get() as u64
+    });
+
+    COUNT_THREADS.store(threads, Ordering::Relaxed);
+    COUNT_OBJECTS.store(cli.objects, Ordering::Relaxed);
+    BLOCK.store(cli.block, Ordering::Relaxed);
+    FINAL.store(sum(cli.objects) * threads, Ordering::Relaxed);
+    BARRIER.get_or_init(|| Barrier::new(threads as usize));
+
+    let pool = Pool::create::<Queue<u64>, Mmt>(&cli.path, cli.size, threads as usize).unwrap();
 
     pool.execute::<Queue<u64>, Mmt>();
-    assert_eq!(GLOBAL.load(Ordering::Relaxed), sum(OBJECTS) * THREADS);
+    assert_eq!(
+        GLOBAL.load(Ordering::Relaxed),
+        FINAL.load(Ordering::Relaxed),
+    );
 }
 
 #[derive(Memento, Default, Collectable)]
@@ -63,18 +88,22 @@ struct Mmt {
     deq: Dequeue<u64>,
 }
 
-static CRASH_PROCESS: AtomicU64 = AtomicU64::new(0);
-static CRASH_THREAD: OnceLock<Vec<u64>> = OnceLock::new();
+static CRASH_THREAD: AtomicUsize = AtomicUsize::new(0);
+static CRASH: Mutex<Vec<u64>> = Mutex::new(Vec::new());
+
+static COUNT_THREADS: AtomicU64 = AtomicU64::new(0);
+static COUNT_OBJECTS: AtomicU64 = AtomicU64::new(0);
+static BLOCK: AtomicBool = AtomicBool::new(false);
+
+static STOP: AtomicBool = AtomicBool::new(false);
+static BARRIER: OnceLock<Barrier> = OnceLock::new();
+
+static FINAL: AtomicU64 = AtomicU64::new(0);
+static GLOBAL: AtomicU64 = AtomicU64::new(0);
 
 thread_local! {
     static LOCAL: Cell<u64> = const { Cell::new(0) };
 }
-static GLOBAL: AtomicU64 = AtomicU64::new(0);
-static FINAL: u64 = sum(OBJECTS) * THREADS;
-static BARRIER: Barrier = Barrier::new(THREADS as usize);
-
-const OBJECTS: u64 = 1000;
-const THREADS: u64 = 8;
 
 const fn sum(i: u64) -> u64 {
     let mut j = 0;
@@ -88,26 +117,71 @@ const fn sum(i: u64) -> u64 {
 
 impl RootObj<Mmt> for Queue<u64> {
     fn run(&self, mmt: &mut Mmt, handle: &Handle) {
+        let block = BLOCK.load(Ordering::Relaxed);
+        let crash_thread = CRASH_THREAD.load(Ordering::Relaxed);
+        let (recover, crash) = if handle.tid == crash_thread {
+            let poisoned = CRASH.is_poisoned();
+            CRASH.clear_poison();
+            (poisoned, &mut *CRASH.lock().unwrap())
+        } else {
+            (false, &mut Vec::new())
+        };
+        let objects = COUNT_OBJECTS.load(Ordering::Relaxed);
+
         let mut i = 0;
 
-        let crash_process = CRASH_PROCESS.load(Ordering::Relaxed);
-        let crash_thread = CRASH_THREAD.get_or_init(Vec::new);
+        if recover && block {
+            STOP.store(true, Ordering::Release);
+            BARRIER.get().unwrap().wait();
+            unsafe {
+                PMEMAllocator::gc();
+                PMEMAllocator::invalidate();
+            }
+            STOP.store(false, Ordering::Release);
+            BARRIER.get().unwrap().wait();
+        }
 
-        while i < OBJECTS {
+        while i < objects {
             self.enqueue(i, &mut mmt.enq, handle);
             i = mmt.i.checkpoint(|| i + 1, handle);
 
-            if i == crash_process {
-                BARRIER.wait();
-                std::process::abort();
-            } else if crash_thread.contains(&i) {
-                panic!()
+            // Check for GC request
+            if handle.tid != crash_thread {
+                if !STOP.load(Ordering::Relaxed) {
+                    continue;
+                }
+                BARRIER.get().unwrap().wait();
+                BARRIER.get().unwrap().wait();
+                unsafe {
+                    PMEMAllocator::invalidate();
+                }
+            }
+
+            if crash.last().copied() == Some(i) {
+                crash.pop();
+                match BLOCK.load(Ordering::Relaxed) {
+                    false => {
+                        println!("{}", unsafe { PMEMAllocator::measure() });
+                        panic!();
+                    }
+                    true => {
+                        panic!();
+                    }
+                }
             }
         }
 
+        let r#final = FINAL.load(Ordering::Relaxed);
+
         loop {
             match self.dequeue(&mut mmt.deq, handle) {
-                None if LOCAL.get() == 0 && GLOBAL.load(Ordering::Acquire) == FINAL => break,
+                None if LOCAL.get() == 0 => {
+                    if GLOBAL.load(Ordering::Relaxed) == r#final {
+                        break;
+                    } else {
+                        std::hint::spin_loop();
+                    }
+                }
                 None => {
                     let local = LOCAL.get();
                     GLOBAL.fetch_add(local, Ordering::AcqRel);
@@ -115,6 +189,17 @@ impl RootObj<Mmt> for Queue<u64> {
                     std::hint::spin_loop();
                 }
                 Some(i) => LOCAL.set(LOCAL.get() + i),
+            }
+
+            if handle.tid != crash_thread {
+                if !STOP.load(Ordering::Relaxed) {
+                    continue;
+                }
+                BARRIER.get().unwrap().wait();
+                BARRIER.get().unwrap().wait();
+                unsafe {
+                    PMEMAllocator::invalidate();
+                }
             }
         }
     }
