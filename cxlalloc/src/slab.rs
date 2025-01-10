86ca0dac295faebc76050992346e6d30173a4dd4
Newton Ni <nwtnni@gmail.com>
2025-01-10 10:54:25 -0600

    Split stacks into separate file

--- cxlalloc/src/slab.rs
+++ cxlalloc/src/slab.rs
@@ -1,8 +1,11 @@
 pub(crate) mod owned;
 pub(crate) mod shared;
+pub(crate) mod stack;
 
 pub(crate) use owned::Owned;
 pub(crate) use shared::Shared;
+pub(crate) use stack::GlobalStack;
+pub(crate) use stack::LocalStack;
 
 use core::alloc::Layout;
 use core::alloc::LayoutError;
@@ -19,13 +22,7 @@ use core::ptr::NonNull;
 
 use ribbit::private::u12;
 
-use crate::atomic::Version;
 use crate::bitset::Bit;
-use crate::cas;
-use crate::cas::help;
-use crate::log::GlobalToLocal;
-use crate::log::LocalToGlobal;
-use crate::log::StateUnpacked;
 use crate::raw;
 use crate::size;
 use crate::thread;
@@ -227,111 +224,6 @@ impl<S> core::ops::Index<Index> for Slice<'_, S> {
     fn index(&self, index: Index) -> &Self::Output {
         unsafe { self.base.add(index._0().get() as usize).as_ref() }
     }
-}
-
-#[repr(C)]
-pub(crate) struct LocalStack {
-    head: Option<Index>,
-    count: usize,
-}
-
-impl LocalStack {
-    pub(crate) fn peek(&self) -> Option<Index> {
-        self.head
-    }
-
-    pub(crate) fn len(&self) -> usize {
-        self.count
-    }
-
-    pub(crate) fn set(&mut self, head: Option<Index>, count: usize) {
-        self.count = count;
-        self.head = head;
-        crate::flush(&self, false);
-    }
-
-    pub(crate) fn pop(&mut self, slabs: &Slice<Owned>) -> Option<Index> {
-        let index = self.head?;
-        self.count -= 1;
-        self.head = slabs[index].next.load();
-        crate::flush(self, false);
-        Some(index)
-    }
-
-    pub(crate) fn push(&mut self, slabs: &Slice<Owned>, index: Index) {
-        let slab = &slabs[index];
-        slab.next.store(self.head);
-        crate::flush(&slab.next, false);
-
-        self.count += 1;
-        self.head = Some(index);
-        crate::flush(&self.head, false);
-    }
-
-    pub(crate) fn trace<'a>(&self, slabs: &'a Slice<Owned>) -> impl Iterator<Item = Index> + 'a {
-        slabs.trace(self.head)
-    }
-}
-
-#[repr(C)]
-pub(crate) struct GlobalStack {
-    head: cas::Detectable<Option<Index>>,
-}
-
-impl GlobalStack {
-    pub(crate) fn push(
-        &self,
-        id: thread::Id,
-        slabs: &Slice<Owned>,
-        help: &help::Array,
-        head: Index,
-        tail: Index,
-    ) {
-        self.head.update(help, id, |old, version| {
-            slabs[tail].next.store(old);
-            crate::flush(&slabs[tail].next, false);
-            Some((
-                Some(head),
-                StateUnpacked::LocalToGlobal(LocalToGlobal::new(head, version)),
-            ))
-        });
-    }
-
-    pub(crate) fn pop(
-        &self,
-        id: thread::Id,
-        slabs: &Slice<Owned>,
-        help: &help::Array,
-    ) -> Option<Index> {
-        self.head
-            .update(help, id, |old, version| {
-                let old = old?;
-                let new = slabs[old].next.load();
-
-                Some((
-                    new,
-                    StateUnpacked::GlobalToLocal(GlobalToLocal::new(old, version)),
-                ))
-            })
-            .flatten()
-    }
-
-    pub(crate) fn is_empty(&self, help: &help::Array) -> bool {
-        self.head.load(help).is_none()
-    }
-}
-
-#[ribbit::pack(size = 64, debug)]
-#[derive(Copy, Clone, PartialEq, Eq)]
-struct Head {
-    #[ribbit(size = 16, nonzero)]
-    id: thread::Id,
-
-    #[ribbit(size = 16)]
-    version: Version,
-
-    #[ribbit(size = 32)]
-    index: Option<Index>,
 }
 
 #[inline]
