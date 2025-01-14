1e17a19562736daa815e7deb57e0d79dcab72e59
Newton Ni <nwtnni@gmail.com>
2025-01-14 11:40:39 -0600

    Add huge allocation test

--- cxlalloc/tests/sequential.rs
+++ cxlalloc/tests/sequential.rs
@@ -3,6 +3,8 @@ use core::ptr::NonNull;
 use cxlalloc::raw;
 use cxlalloc::Allocator;
 
+const PAGE: usize = 4096;
+
 fn with_allocator<F: FnOnce(&mut Allocator)>(apply: F) {
     let _ = env_logger::try_init();
     let raw = raw::Builder::default().build("").unwrap();
@@ -17,7 +19,7 @@ fn create() {
 }
 
 #[test]
-fn allocate_small() {
+fn small() {
     with_allocator(|allocator| unsafe {
         let small = allocator
             .allocate_untyped(8)
@@ -29,5 +31,24 @@ fn allocate_small() {
         assert_eq!(*small, 5);
 
         allocator.free_untyped(NonNull::from(small).cast());
+    })
+}
+
+#[test]
+fn huge() {
+    with_allocator(|allocator| unsafe {
+        const SIZE: usize = 1 << 30;
+
+        let huge = allocator
+            .allocate_untyped(SIZE)
+            .cast::<[u8; SIZE]>()
+            .as_mut()
+            .unwrap();
+
+        for i in 0..SIZE / PAGE {
+            huge[i * PAGE] = i as u8;
+        }
+
+        allocator.free_untyped(NonNull::from(huge).cast());
     })
 }
