c6cf2543bf1fb51853555c67f5f019be2884dfcb
Newton Ni <nwtnni@gmail.com>
2025-03-05 15:41:02 -0600

    Plot both YCSB experiments

--- plot/bench.py
+++ plot/bench.py
@@ -6,26 +6,66 @@ import plotly.express as px
 def main():
     df = pl.read_ndjson(sys.argv[1])
 
+    fig = None
+    if "load" in sys.argv[1]:
+        fig = plot_load(df)
+    else:
+        fig = plot_insert_proportion(df)
+
+    fig.show()
+
+
+def plot_load(df):
     df = (
-        df.group_by("benchmark", "allocator", "process_count", "thread_count")
-        .agg(pl.col("time").max() / 1e6)
-        .sort("benchmark", "process_count", "thread_count")
-        .with_columns(thread_total=pl.col("process_count") * pl.col("thread_count"))
+        df.group_by("allocator", "control", "ycsb")
+        .agg(
+            time_mean=pl.col("time").mean() / 1e6,
+            time_std=pl.col("time").std() / 1e6,
+        )
+        .with_columns(
+            thread_total=pl.col("control").struct["process_count"]
+            * pl.col("control").struct["thread_count"]
+        )
+        .sort("allocator", "thread_total")
     )
 
     fig = px.line(
         df,
         x="thread_total",
-        y="time",
+        y="time_mean",
+        # error_y="time_std",
         color="allocator",
-        facet_col="process_count",
-        facet_row="benchmark",
         markers=True,
     )
 
     fig.update_xaxes(title_text="Thread Count", tickvals=df["thread_total"].unique())
-    fig.update_yaxes(title_text="Time (s)")
-    fig.show()
+    fig.update_yaxes(title_text="Time (s)", autorangeoptions_include=0.0)
+    return fig
+
+
+def plot_insert_proportion(df):
+    df = (
+        df.group_by("allocator", "control", "ycsb")
+        .agg(
+            pl.col("ycsb").struct["insert_proportion"].first(),
+            pl.col("time").max() / 1e6,
+        )
+        .sort("allocator", "insert_proportion")
+    )
+
+    fig = px.line(
+        df,
+        x="insert_proportion",
+        y="time",
+        color="allocator",
+        markers=True,
+    )
+
+    fig.update_xaxes(
+        title_text="Insert Proportion", tickvals=df["insert_proportion"].unique()
+    )
+    fig.update_yaxes(title_text="Time (s)", autorangeoptions_include=0.0)
+    return fig
 
 
 if __name__ == "__main__":
