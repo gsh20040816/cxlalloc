73003ddc486ea02aee7865668bea995335c95dfa
Newton Ni <nwtnni@gmail.com>
2025-03-18 14:15:28 -0500

    Implement process-global Rust wrapper around cxlalloc

---
+++ cxlalloc-global/src/lib.rs
@@ -0,0 +1,72 @@
+use core::cell::Cell;
+use core::cell::RefCell;
+use core::ffi;
+use core::mem;
+use core::num::NonZeroU64;
+use core::ptr;
+use core::ptr::NonNull;
+use std::sync::OnceLock;
+
+use cxlalloc::Allocator;
+
+static RAW: OnceLock<cxlalloc::Raw> = OnceLock::new();
+
+thread_local! {
+    static THREAD_ID: Cell<u16> = const { Cell::new(0) };
+    static ALLOCATOR: RefCell<cxlalloc::Allocator<'static, (), ()>> = RefCell::new(RAW.get().unwrap().allocator(unsafe {
+        cxlalloc::thread::Id::new(THREAD_ID.get())
+    }));
+}
+
+pub fn initialize_process(builder: cxlalloc::raw::Builder, name: &str) {
+    RAW.get_or_init(|| builder.build(name).unwrap());
+
+    let mut action = unsafe { mem::zeroed::<libc::sigaction>() };
+    action.sa_sigaction = handle_sigsegv as _;
+    action.sa_flags = libc::SA_SIGINFO | libc::SA_NODEFER;
+    unsafe {
+        libc::sigaction(libc::SIGSEGV, &action, ptr::null_mut());
+    }
+}
+
+pub fn initialize_thread(id: u16) {
+    THREAD_ID.set(id);
+    ALLOCATOR.with(|_| ())
+}
+
+pub fn allocate_untyped(size: usize) -> Option<NonNull<ffi::c_void>> {
+    NonNull::new(with(|allocator| allocator.allocate_untyped(size)))
+}
+
+pub fn free_untyped(pointer: *mut ffi::c_void) {
+    let Some(pointer) = NonNull::new(pointer) else {
+        return;
+    };
+    with(|allocator| allocator.free_untyped(pointer))
+}
+
+pub fn pointer_to_offset(pointer: NonNull<ffi::c_void>) -> usize {
+    with(|allocator| allocator.pointer_to_offset(pointer))
+}
+
+pub fn offset_to_pointer(offset: usize) -> NonNull<ffi::c_void> {
+    with(|allocator| allocator.offset_to_pointer(offset))
+}
+
+fn handle_sigsegv(_: libc::c_int, info: *const libc::siginfo_t, _: *const libc::c_void) {
+    let address = unsafe { info.read().si_addr() };
+
+    if RAW.get().unwrap().map(address) {
+        return;
+    }
+
+    unsafe {
+        let mut action = mem::zeroed::<libc::sigaction>();
+        action.sa_sigaction = libc::SIG_DFL;
+        libc::sigaction(libc::SIGSEGV, &action, ptr::null_mut());
+    }
+}
+
+fn with<F: FnOnce(&mut Allocator<'static, (), ()>) -> T, T>(apply: F) -> T {
+    ALLOCATOR.with_borrow_mut(|allocator| apply(allocator))
+}
