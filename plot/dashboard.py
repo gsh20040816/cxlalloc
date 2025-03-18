b2983c33f2ac0eaf081e50839d2907bdbfcee2cb
Newton Ni <nwtnni@gmail.com>
2025-03-18 16:11:55 -0500

    Extend dashboard to select output
    Not sure if it's possible to detect which fields are output without
    assuming something about the structure of the data.

--- plot/dashboard.py
+++ plot/dashboard.py
@@ -8,18 +8,15 @@ import polars as pl
 from polars import selectors as cs
 
 
-df = pl.read_ndjson(sys.argv[1])
+DF = pl.read_ndjson(sys.argv[1])
 
-TYPE_VARYING = "varying"
+
+TYPE_COL = "col"
 TYPE_STORE = "store"
-
 ID_FIGURE = "figure"
 
-OUTPUTS = ["process_id", "thread_id", "time"]
-VARYING = []
-
-
-CHOICES = [
+COLS = []
+CHOICES_INDEPENDENT = [
     {"label": label, "value": value}
     for label, value in [
         ("Set as X-axis", "x"),
@@ -29,47 +26,84 @@ CHOICES = [
         ("Ignore", "ignore"),
     ]
 ]
+CHOICES_DEPENDENT = [
+    {"label": label, "value": value}
+    for label, value in [
+        ("Set as Y-axis", "y"),
+        ("Ignore", "ignore"),
+    ]
+]
+
+
+class Col:
+    def __init__(self, name: str, selector):
+        self.name = name
+        self.selector = selector
+
+    def store(self):
+        return dcc.Store(
+            id={"type": TYPE_STORE, "index": self.name},
+            storage_type="local",
+        )
+
+    # ID used in pattern matching callback
+    # https://dash.plotly.com/pattern-matching-callbacks
+    def id(self):
+        return {"type": TYPE_COL, "index": self.name}
 
 
 def main():
-    fixed = []
-    varying = []
+    ui_control = [html.H2("Control")]
+    ui_independent = [html.H2("Independent")]
+    ui_dependent = [html.H2("Dependent")]
 
-    for name, selector in flatten(df.drop(OUTPUTS)):
-        values = unique(selector)
+    for col in flatten(DF):
+        if col.name.startswith("output"):
+            COLS.append(col)
+            ui_dependent.append(
+                dbc.Row(
+                    [
+                        col.store(),
+                        dbc.Col(html.Span(col.name)),
+                        dbc.Col(
+                            dcc.Dropdown(CHOICES_DEPENDENT, id=col.id()),
+                        ),
+                    ]
+                )
+            )
+            continue
+
+        values = unique(col.selector)
 
         if len(values) == 1:
             value = values[0]
             if type(value) is bool:
                 value = "true" if value else "false"
 
-            fixed.append(
+            ui_control.append(
                 dbc.Row(
                     [
-                        dbc.Col(html.Span(name)),
+                        dbc.Col(html.Span(col.name)),
                         dbc.Col(dcc.Dropdown([value], value=value, disabled=True)),
                     ]
                 )
             )
             continue
 
-        VARYING.append((name, selector))
-        varying.append(
+        COLS.append(col)
+        ui_independent.append(
             dbc.Row(
                 [
-                    dcc.Store(
-                        id={"type": TYPE_STORE, "index": name},
-                        storage_type="local",
-                    ),
-                    dbc.Col(html.Span(name)),
+                    col.store(),
+                    dbc.Col(html.Span(col.name)),
                     dbc.Col(
                         dcc.Dropdown(
-                            CHOICES
+                            CHOICES_INDEPENDENT
                             + [
                                 {"label": f"Filter to {value}", "value": value}
                                 for value in values.to_list()
                             ],
-                            id={"type": TYPE_VARYING, "index": name},
+                            id=col.id(),
                         )
                     ),
                 ]
@@ -80,10 +114,13 @@ def main():
         external_stylesheets=[dbc.themes.BOOTSTRAP],
     )
     app.layout = [
+        dbc.Row(html.H1(sys.argv[1])),
+        dbc.Row(html.Hr()),
         dbc.Row(
             [
-                dbc.Col(fixed),
-                dbc.Col(varying),
+                dbc.Col(ui_control),
+                dbc.Col(ui_independent),
+                dbc.Col(ui_dependent),
             ]
         ),
         dcc.Graph(figure={}, id=ID_FIGURE),
@@ -106,26 +143,26 @@ def flatten(df):
                     select(col),
                 )
             else:
-                yield (name, select(col))
+                yield Col(name, select(col))
 
     yield from recurse(df.columns, "", None)
 
 
-def unique(field):
-    return df.select(field).unique().to_series().sort()
+def unique(selector):
+    return DF.select(selector).unique().to_series().sort()
 
 
 @callback(
     Output({"type": TYPE_STORE, "index": dash.MATCH}, "data"),
-    Output({"type": TYPE_VARYING, "index": dash.MATCH}, "value"),
+    Output({"type": TYPE_COL, "index": dash.MATCH}, "value"),
     Input({"type": TYPE_STORE, "index": dash.MATCH}, "data"),
-    Input({"type": TYPE_VARYING, "index": dash.MATCH}, "value"),
+    Input({"type": TYPE_COL, "index": dash.MATCH}, "value"),
 )
-def update_store(data, varying):
-    if varying is None:
-        return data, data
+def sync_store(store, ui):
+    if ui is None:
+        return store, store
     else:
-        return varying, varying
+        return ui, ui
 
 
 @callback(
@@ -135,71 +172,77 @@ def update_store(data, varying):
 )
 def update(
     ts,
-    varying,
+    values,
 ):
-    if ts is None or any([value is None for value in varying]):
+    if ts is None or any([value is None for value in values]):
         raise dash.exceptions.PreventUpdate
 
     x = None
+    y = None
     facet_row = None
     facet_column = None
     facet_color = None
     filters = []
 
     # Validate
-    for (name, selector), value in zip(VARYING, varying):
+    for col, value in zip(COLS, values):
         if value == "x":
             if x is not None:
                 return {}
-            x = (name, selector)
+            x = col
+        elif value == "y":
+            if y is not None:
+                return {}
+            y = col
         elif value == "facet_row":
             if facet_row is not None:
                 return {}
-            facet_row = (name, selector)
+            facet_row = col
         elif value == "facet_column":
             if facet_column is not None:
                 return {}
-            facet_column = (name, selector)
+            facet_column = col
         elif value == "facet_color":
             if facet_color is not None:
-                return None
-            facet_color = (name, selector)
+                return {}
+            facet_color = col
         elif value == "ignore":
             continue
-        else:
-            filters.append(selector == value)
+        elif value is not None:
+            filters.append(col.selector == value)
 
-    filtered = df.filter(*filters)
+    if x is None or y is None:
+        raise dash.exceptions.PreventUpdate
 
-    sorts = [x[0]]
+    filtered = DF.filter(*filters)
+
+    sorts = [x.name]
     cols = [
-        x[1].first().alias(x[0]),
-        pl.col("time").mean().alias("time_mean") / 1e6,
-        pl.col("time").std().alias("time_std") / 1e6,
+        x.selector.first().alias(x.name),
+        y.selector.mean().alias(f"{y.name}_mean"),
+        y.selector.std().alias(f"{y.name}_std"),
     ]
 
-    for name, selector in [
-        v for v in [facet_row, facet_column, facet_color] if v is not None
-    ]:
-        sorts.append(name)
-        if name not in filtered.columns:
-            cols.append(selector.first().alias(name))
+    for col in [v for v in [facet_row, facet_column, facet_color] if v is not None]:
+        sorts.append(col.name)
+        if col.name not in filtered.columns:
+            cols.append(col.selector.first().alias(col.name))
 
-    filtered = filtered.group_by(cs.exclude(*OUTPUTS)).agg(cols).sort(sorts)
+    filtered = filtered.group_by(cs.exclude("output")).agg(cols).sort(sorts)
 
     fig = px.line(
         filtered,
-        x=x[0],
-        y="time_mean",
-        error_y="time_std",
+        x=x.name,
+        y=f"{y.name}_mean",
+        error_y=f"{y.name}_std",
         facet_row=facet_row[0] if facet_row is not None else None,
         facet_col=facet_column[0] if facet_column is not None else None,
         color=facet_color[0] if facet_color is not None else None,
         markers=True,
     )
 
-    fig.update_xaxes(title_text=x[0], tickvals=filtered[x[0]].unique())
-    fig.update_yaxes(title_text="Time (s)", autorangeoptions_include=0.0)
+    fig.update_xaxes(title_text=x.name, tickvals=filtered[x.name].unique())
+    fig.update_yaxes(title_text=y.name, autorangeoptions_include=0.0)
     return fig
 
 
