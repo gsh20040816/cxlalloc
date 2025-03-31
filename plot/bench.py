ec4ce26a34c7a3dcab61a7864ae3ec9d339b6b8a
Newton Ni <nwtnni@gmail.com>
2025-03-31 16:22:58 -0500

    Try to remove redundant labels

--- plot/bench.py
+++ plot/bench.py
@@ -20,6 +20,9 @@ FILTER_MEMCACHED = (pl.col("allocator") == "cxl_shm") & pl.col(
     ]
 )
 
+THROUGHPUT = "output/throughput"
+MAX_RSS = "output/resource_usage/max_rss"
+
 
 def main():
     alt.renderers.enable("browser")
@@ -28,19 +31,20 @@ def main():
     df = (
         unnest_all(df, "/")
         .sort("allocator", "config_benchmark/trace", "config_global/thread_count")
+        .filter(pl.col("config_global/thread_count").is_in([1, 4, 16, 32]))
         .with_columns(
             pl.when(FILTER_MEMCACHED)
             .then(0)
-            .otherwise(pl.col("output/throughput"))
-            .alias("output/throughput")
+            .otherwise(pl.col(THROUGHPUT))
+            .alias(THROUGHPUT),
+            pl.when(FILTER_MEMCACHED).then(0).otherwise(pl.col(MAX_RSS)).alias(MAX_RSS),
         )
     )
 
     baseline = (
         df.filter(pl.col("allocator") == "cxlalloc")
-        .select("output/throughput")
+        .select(THROUGHPUT, MAX_RSS)
         .collect()
-        .to_series(0)
     )
 
     df = (
@@ -48,16 +52,47 @@ def main():
         .agg(
             trace=pl.col("config_benchmark/trace"),
             thread_count=pl.col("config_global/thread_count"),
-            throughput=pl.col("output/throughput"),
-            throughput_relative=pl.col("output/throughput") / baseline,
+            throughput=pl.col(THROUGHPUT),
+            throughput_relative=pl.col(THROUGHPUT) / baseline.get_column(THROUGHPUT),
+            max_rss=pl.col(MAX_RSS),
+            max_rss_relative=pl.col(MAX_RSS) / baseline.get_column(MAX_RSS),
         )
-        .explode(["trace", "thread_count", "throughput", "throughput_relative"])
+        .explode(
+            [
+                "trace",
+                "thread_count",
+                "throughput",
+                "throughput_relative",
+                "max_rss",
+                "max_rss_relative",
+            ]
+        )
         .collect()
     )
 
-    base = alt.Chart(df).encode(
-        x="thread_count:N",
-        y=alt.Y("throughput").axis(format="s"),
+    tput = plot(df, "throughput", True)
+    rss = plot(df, "max_rss", False)
+
+    chart = alt.vconcat(
+        tput,
+        rss,
+        title=alt.Title("Memcached Workloads", anchor="middle"),
+    )
+
+    chart.show()
+
+
+def plot(df, metric: str, top: bool):
+    y = alt.Y(metric).axis(format="s", title=None)
+
+    if metric == "max_rss":
+        y = y.scale(alt.Scale(domain=[0, 14 * 2**30], clamp=True))
+
+    base = alt.Chart(df, width=alt.Step(10)).encode(
+        x=alt.X("thread_count:N", title=None if top else "Thread Count").axis(
+            labels=False if top else True
+        ),
+        y=y,
         xOffset=alt.XOffset("allocator", sort=SORT),
     )
 
@@ -66,15 +101,15 @@ def main():
     absolute = (
         base.transform_filter(alt.datum.allocator == "cxlalloc")
         .mark_text(align="left", angle=270, dx=5)
-        .encode(text=alt.Text("throughput", format=".2s"))
+        .encode(text=alt.Text(metric, format=".2s"))
     )
 
     relative = (
         base.transform_filter(alt.datum.allocator != "cxlalloc")
         .transform_calculate(
             text=alt.expr.if_(
-                alt.datum.throughput > 0,
-                alt.expr.format(alt.datum.throughput_relative, ".2f") + "x",
+                alt.datum[metric] > 0,
+                alt.expr.format(alt.datum[metric + "_relative"], ".2r") + "x",
                 "X",
             ),
         )
@@ -82,14 +117,22 @@ def main():
         .encode(
             text="text:N",
             color=alt.condition(
-                alt.datum.throughput > 0,
+                alt.datum[metric] > 0,
                 alt.value("black"),
                 alt.value("red"),
             ),
         )
     )
 
-    (bar + absolute + relative).facet(row="trace").resolve_scale(y="independent").show()
+    chart = (bar + absolute + relative).properties(
+        height=100 if metric == "max_rss" else 200
+    )
+    column = alt.Column(
+        "trace",
+        header=alt.Header(title=metric, titleOrient="left", labels=top),
+    )
+
+    return chart.facet(column=column).resolve_scale(y="independent")
 
 
 # https://github.com/pola-rs/polars/issues/12353
