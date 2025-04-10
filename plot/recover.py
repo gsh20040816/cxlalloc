d165cc91549cda0b0cf2c1e969d62a00c75cdd83
Newton Ni <nwtnni@gmail.com>
2025-04-09 23:42:59 -0500

    Checkpoint recovery plot

--- plot/recover.py
+++ plot/recover.py
@@ -1,125 +1,153 @@
-import altair as alt
-import common
 import polars as pl
-import polars.selectors as cs
+import plotly
+import plotly.graph_objects as go
+import plotly.subplots as sp
 import sys
 
 ALLOCATOR = "Allocator"
 CRASH_COUNT = "Crash Count"
 PHASE = "Phase"
-PHASE_GC = "Garbage Collection"
+PHASE_GC = "GC"
 PHASE_EXEC = "Execution"
 WORKLOAD = "Workload"
 LEAK_SIZE = "Leak Size"
 TIME = "Execution Time (s)"
 TIME_TOTAL = "Total Time"
 
-ORDER_ALLOCATOR = ["cxlalloc", "ralloc-leak", "ralloc-block"]
+ORDER_ALLOCATOR = ["cxlalloc", "ralloc-leak", "ralloc-gc"]
 ORDER_PHASE = [PHASE_GC, PHASE_EXEC]
+
+THEME = "plotly_white"
+SCHEME = plotly.colors.qualitative.D3
+PATTERN = {
+    PHASE_GC: "x",
+    PHASE_EXEC: "",
+}
+COLOR_ALLOCATOR = {
+    "cxlalloc": "black",
+    "ralloc-leak": SCHEME[1],
+    "ralloc-gc": SCHEME[-1],
+}
 
 
 def main():
-    alt.renderers.enable("browser")
-
     df = pl.scan_ndjson(sys.argv[1])
 
     df = (
-        common.unnest_all(df)
-        .select(
-            pl.when(pl.col("config/allocator") == "cxlalloc")
+        df.select(
+            pl.when(pl.col("config").struct["allocator"] == "cxlalloc")
             .then(pl.lit("cxlalloc"))
-            .when((pl.col("config/allocator") == "ralloc") & pl.col("config/block"))
+            .when(
+                (pl.col("config").struct["allocator"] == "ralloc")
+                & pl.col("config").struct["block"]
+            )
             .then(pl.lit("ralloc-gc"))
             .otherwise(pl.lit("ralloc-leak"))
             .alias(ALLOCATOR),
-            pl.col("config/workload").alias(WORKLOAD),
-            pl.col("config/crash_count").alias(CRASH_COUNT),
-            pl.col("output/time").alias(TIME_TOTAL),
-            pl.col("output/gc_time").alias(PHASE_GC),
-            (pl.col("output/time") - pl.col("output/gc_time")).alias(PHASE_EXEC),
-            pl.col("output/cache_size").alias(LEAK_SIZE),
+            pl.col("config").struct["workload"].alias(WORKLOAD),
+            pl.col("config").struct["crash_count"].alias(CRASH_COUNT),
+            pl.col("output").struct["time"].alias(TIME_TOTAL),
+            pl.col("output").struct["gc_time"].alias(PHASE_GC),
+            pl.col("output").struct["cache_size"].alias(LEAK_SIZE),
         )
         .filter(pl.col(CRASH_COUNT).is_in([0, 1, 2]))
-        .unpivot(
-            index=cs.exclude(PHASE_GC, PHASE_EXEC),
-            variable_name=PHASE,
-            value_name=TIME,
-        )
         .with_columns(
             pl.col(LEAK_SIZE) / 2**10,
             pl.col(TIME_TOTAL) / 1e6,
-            pl.col(TIME) / 1e6,
+            pl.col(PHASE_GC) / 1e6,
         )
     )
 
-    charts = []
-
-    workloads = (
-        df.select(WORKLOAD).unique(maintain_order=True).collect().get_column(WORKLOAD)
+    workloads = df.select(WORKLOAD).unique(maintain_order=True).collect().to_series()
+    crash_counts = (
+        df.select(CRASH_COUNT).unique(maintain_order=True).collect().to_series().sort()
     )
+
+    fig = sp.make_subplots(
+        rows=1,
+        cols=2,
+        x_title="Crash Count (threads)",
+        column_titles=workloads.to_list(),
+    )
+    gc_legend = False
+    allocator_legend = False
 
     for col, workload in enumerate(workloads):
-        data = df.filter(pl.col(WORKLOAD) == workload).collect()
+        for offset, allocator in enumerate(ORDER_ALLOCATOR):
+            data = df.filter(
+                (pl.col(WORKLOAD) == workload) & (pl.col(ALLOCATOR) == allocator)
+            ).collect()
 
-        base = alt.Chart(data).encode(
-            x=alt.X(f"{CRASH_COUNT}:O", title=None),
-            y=alt.Y(f"{TIME}:Q", title=TIME if col == 0 else None),
-            xOffset=alt.XOffset(f"{ALLOCATOR}:N").sort(ORDER_ALLOCATOR),
-        )
-
-        bar = base.mark_bar().encode(
-            color=alt.Color(f"{ALLOCATOR}:N").sort(ORDER_ALLOCATOR),
-            fillOpacity=alt.FillOpacity(PHASE).sort(ORDER_PHASE),
-        )
-
-        leak = (
-            base.transform_filter(
-                (alt.datum[ALLOCATOR] == "ralloc-leak")
-                & (alt.datum[PHASE] == PHASE_EXEC)
+            trace = go.Bar(
+                name=allocator,
+                x=data[CRASH_COUNT],
+                y=data[TIME_TOTAL],
+                legendgroup=allocator,
+                legendgrouptitle_text="Allocator" if not allocator_legend else None,
+                marker_color=COLOR_ALLOCATOR[allocator],
             )
-            .transform_calculate(
-                text=alt.expr.if_(
-                    alt.datum[LEAK_SIZE] > 0,
-                    alt.expr.format(alt.datum[LEAK_SIZE], ".3s") + " KiB Leaked",
-                    "",
-                )
-            )
-            .mark_text(align="left", angle=270, dx=5)
-            .encode(text="text:N")
-        )
+            allocator_legend = True
 
-        gc = (
-            base.transform_filter(
-                (alt.datum[ALLOCATOR] == "ralloc-gc") & (alt.datum[PHASE] == PHASE_GC)
-            )
-            .transform_calculate(
-                text=alt.expr.if_(
-                    alt.datum[TIME] > 0,
-                    alt.expr.format(alt.datum[TIME] / alt.datum[TIME_TOTAL], ".2p")
-                    + " in GC",
-                    "",
-                )
-            )
-            .mark_text(align="left", angle=270, dx=5)
-            .encode(y=alt.value(200), text="text:N")
-        )
+            fig.add_trace(trace, row=1, col=col + 1)
 
-        inner = alt.layer(bar + leak + gc, title=alt.Title(workload)).properties(
-            height=200, width=200
-        )
-        charts.append(inner)
+            for row, crash_count in enumerate(crash_counts):
+                filtered = data.filter(pl.col(CRASH_COUNT) == crash_count)
+                gc = filtered[PHASE_GC].item()
+                total = filtered[TIME_TOTAL].item()
+                leak = filtered[LEAK_SIZE].item()
 
-    bar = alt.vconcat(
-        alt.hconcat(
-            *charts,
-            title=alt.Title("Memento Workloads", align="center", anchor="middle"),
-        ),
-        alt.hconcat(
-            title=alt.Title("Thread Crash Count", align="center", anchor="middle")
-        ),
-        center=True,
-    ).configure_legend(orient="bottom")
-    bar.show()
+                if gc > 0:
+                    fig.add_shape(
+                        name=PHASE_GC,
+                        showlegend=not gc_legend,
+                        legendgroup="Phase",
+                        legendgrouptitle_text="Phase",
+                        type="rect",
+                        xref=f"x{col + 1}",
+                        x0=row + 0.125,
+                        x1=row + 0.375,
+                        yref=f"y{col + 1}",
+                        y0=0.0,
+                        y1=gc,
+                        line_color="black",
+                        # fillcolor="red",
+                        # opacity=0.2,
+                        # line_width=0,
+                        label=dict(
+                            text=f"GC {gc / total * 100.0:.0f}%",
+                            textangle=-90,
+                            font_color="red",
+                        ),
+                    )
+                    gc_legend = True
+
+                if leak > 0:
+                    fig.add_annotation(
+                        x=row,
+                        y=total + 1,
+                        showarrow=True,
+                        arrowhead=3,
+                        arrowsize=2,
+                        xref=f"x{col + 1}",
+                        text=f"Leak {leak:.1f} KiB",
+                        textangle=-90,
+                        xanchor="left",
+                        font_color="red",
+                    )
+
+    unique = set()
+    # https://stackoverflow.com/a/62162555
+    fig.for_each_trace(
+        lambda trace: trace.update(showlegend=False)
+        if (trace.name in unique)
+        else unique.add(trace.name)
+    )
+
+    fig.update_yaxes(title="Execution Time (secs)", col=1)
+    fig.update_layout(
+        width=600, height=400, title="Memento Partial Failure Workloads", template=THEME
+    )
+    fig.show()
 
 
 if __name__ == "__main__":
