06f47f4066da8c067758fe127e53ebe63c28a590
Newton Ni <nwtnni@gmail.com>
2025-04-10 21:45:09 -0500

    Disable `MPOL_MF_STRICT` again

--- shm/src/lib.rs
+++ shm/src/lib.rs
@@ -232,9 +232,10 @@ impl Raw {
                 libc::MPOL_F_STATIC_NODES | policy,
                 &mask,
                 64,
-                // MPOL_MF_STRICT
+                // MPOL_MF_STRICT sometimes raises EIO when called concurrently for the same
+                // address range, so disable for now.
                 // https://github.com/torvalds/linux/blob/0c559323bbaabee7346c12e74b497e283aaafef5/include/uapi/linux/mempolicy.h#L48
-                1,
+                0,
             )
         } {
             0 => Ok(()),
