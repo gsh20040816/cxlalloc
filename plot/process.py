b571dda27d034aff6d2637bbc90815d2732a0af3
Newton Ni <nwtnni@gmail.com>
2025-04-15 18:19:15 -0500

    Checkpoint process plot

--- plot/process.py
+++ plot/process.py
@@ -1,50 +1,74 @@
 import common
 from common import (
     ALLOCATOR,
-    DATE,
     THROUGHPUT,
-    MAX_RSS,
     PROCESS_COUNT,
     THREAD_COUNT,
     WORKLOAD,
 )
-import sys
 import polars as pl
-import polars.selectors as cs
-import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as sp
+
+MACRO = True
+
+WORKLOADS = common.MACRO_WORKLOADS if MACRO else common.MICRO_WORKLOADS
 
 
 def main():
     df = common.scan_ndjson()
-
-    df = (
-        common.collapse(
-            # df.filter(pl.col("allocator").struct["numa"].struct["policy"] == "bind"),
-            df,
-            workloads=common.HUGE_WORKLOADS,
-        )
-        # .group_by(cs.exclude(PROCESS_COUNT, THROUGHPUT, MAX_RSS))
-        # .agg(THROUGHPUT, MAX_RSS)
-        # .with_columns(
-        #     mean=pl.col(THROUGHPUT).list.mean(),
-        #     std_percent=pl.col(THROUGHPUT).list.std()
-        #     / pl.col(THROUGHPUT).list.mean()
-        #     * 100,
-        # )
-        # .sort("std_percent")
-        .collect()
-    )
-
-    fig = px.line(
+    df = common.collapse(
         df,
-        x=THREAD_COUNT,
-        y=THROUGHPUT,
-        error_y=THROUGHPUT + "_std",
-        color=PROCESS_COUNT,
-        facet_col=WORKLOAD,
-        markers=True,
-        log_y=False,
+        workloads=WORKLOADS,
     )
+
+    thread_counts = df.select(THREAD_COUNT).unique().collect().to_series().sort()
+
+    fig = sp.make_subplots(
+        rows=len(thread_counts),
+        cols=len(WORKLOADS),
+        shared_xaxes=True,
+        column_titles=WORKLOADS,
+        horizontal_spacing=0.03,
+        vertical_spacing=0.10,
+    )
+
+    for col, workload in enumerate(WORKLOADS):
+        for row, thread_count in enumerate(thread_counts):
+            for allocator in common.ALLOCATORS:
+                data = (
+                    df.filter(pl.col(THREAD_COUNT) == thread_count)
+                    .filter(pl.col(ALLOCATOR) == allocator)
+                    .filter(pl.col(WORKLOAD) == workload)
+                    .collect()
+                )
+
+                trace = common.style(
+                    allocator,
+                    go.Scatter,
+                    x=data[PROCESS_COUNT],
+                    y=data[THROUGHPUT],
+                    error_y=dict(array=data[THROUGHPUT + "_std"]),
+                )
+
+                fig.add_trace(trace, row=row + 1, col=col + 1)
+
+    common.update_layout(fig, full=MACRO, numa=False)
+
+    fig.for_each_xaxis(
+        lambda xaxis: xaxis.update(title_text="Process Count"),
+        row=2,
+        col=1 if MACRO else None,
+    )
+
+    for row, thread_count in enumerate(thread_counts):
+        fig.for_each_yaxis(
+            lambda yaxis: yaxis.update(title_text=f"Throughput@{thread_count}T"),
+            row=row + 1,
+            col=1,
+        )
+
+    fig.for_each_yaxis(lambda yaxis: yaxis.update(range=[0, None], rangemode="tozero"))
     fig.show()
     fig.write_image("out.pdf")
 
