92431dd81075e9761168e60bc9256ec72703037f
Newton Ni <nwtnni@gmail.com>
2025-04-15 00:24:39 -0500

    Fix inversion

--- cxlalloc-bench/src/lib.rs
+++ cxlalloc-bench/src/lib.rs
@@ -53,9 +53,9 @@ impl Config {
             allocator_bench::benchmark::Config::Xmalloc(_) => self.global.thread_count & 1 != 0,
 
             allocator_bench::benchmark::Config::Memcached(config) => {
-                !(self.allocator.name == "cxl_shm"
+                self.allocator.name == "cxl_shm"
                     && (config.trace.to_string_lossy().contains("cluster12")
-                        || config.trace.to_string_lossy().contains("cluster37")))
+                        || config.trace.to_string_lossy().contains("cluster37"))
             }
         }
     }
