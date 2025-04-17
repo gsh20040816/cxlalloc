c303f497a0b5cf32559009d8779264aae25c3b33
Newton Ni <nwtnni@gmail.com>
2025-04-17 10:37:57 -0500

    Checkpoint recovery plot

--- plot/recover.py
+++ plot/recover.py
@@ -4,6 +4,7 @@ import plotly.io as pio
 import plotly.graph_objects as go
 import plotly.subplots as sp
 import sys
+import common
 
 # https://github.com/plotly/plotly.py/issues/3469
 pio.kaleido.scope.mathjax = None
@@ -18,19 +19,17 @@ LEAK_SIZE = "Leak Size"
 TIME = "Execution Time (s)"
 TIME_TOTAL = "Total Time"
 
-ORDER_ALLOCATOR = ["cxlalloc", "ralloc-leak", "ralloc-gc"]
+ORDER_ALLOCATOR = ["shmalloc", "ralloc-leak", "ralloc-gc"]
 ORDER_PHASE = [PHASE_GC, PHASE_EXEC]
 
-THEME = "plotly_white"
-SCHEME = plotly.colors.qualitative.D3
 PATTERN = {
     PHASE_GC: "x",
     PHASE_EXEC: "",
 }
 COLOR_ALLOCATOR = {
-    "cxlalloc": "black",
-    "ralloc-leak": SCHEME[1],
-    "ralloc-gc": SCHEME[-1],
+    "shmalloc": "black",
+    "ralloc-leak": common.SCHEME[1],
+    "ralloc-gc": "lightgray",
 }
 
 
@@ -40,7 +39,7 @@ def main():
     df = (
         df.select(
             pl.when(pl.col("config").struct["allocator"] == "cxlalloc")
-            .then(pl.lit("cxlalloc"))
+            .then(pl.lit("shmalloc"))
             .when(
                 (pl.col("config").struct["allocator"] == "ralloc")
                 & pl.col("config").struct["block"]
@@ -70,10 +69,9 @@ def main():
     fig = sp.make_subplots(
         rows=1,
         cols=2,
-        x_title="Crash Count (threads)",
+        # x_title="Crash Count (threads)",
         column_titles=workloads.to_list(),
     )
-    allocator_legend = False
 
     for col, workload in enumerate(workloads):
         for offset, allocator in enumerate(ORDER_ALLOCATOR):
@@ -86,10 +84,8 @@ def main():
                 x=data[CRASH_COUNT],
                 y=data[TIME_TOTAL],
                 legendgroup=allocator,
-                legendgrouptitle_text="Allocator" if not allocator_legend else None,
                 marker_color=COLOR_ALLOCATOR[allocator],
             )
-            allocator_legend = True
 
             fig.add_trace(trace, row=1, col=col + 1)
 
@@ -145,13 +141,34 @@ def main():
         else unique.add(trace.name)
     )
 
-    fig.update_yaxes(title="Execution Time (secs)", col=1)
+    fig.for_each_xaxis(
+        lambda xaxis: xaxis.update(
+            title=dict(text="Crash Count (#threads)", font_size=common.SIZE_XAXIS_TITLE)
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig.update_yaxes(
+        title=dict(text="Execution Time (secs)", font_size=common.SIZE_YAXIS_TITLE),
+        col=1,
+    )
     fig.update_layout(
         width=600,
         height=400,
-        title="Memento Partial Failure Workloads",
-        template=THEME,
-        margin=dict(l=0, r=0, t=50, b=50),
+        # title="Memento Partial Failure Workloads",
+        template=common.THEME,
+        margin=dict(l=0, r=0, t=20, b=50),
+        legend=dict(
+            # title=dict(text="Allocator", font_size=16),
+            font_size=common.SIZE_LEGEND_ENTRY,
+            orientation="h",
+            xanchor="right",
+            yanchor="top",
+            y=-0.06,
+            # y=-0.16,
+            x=1.0,
+        ),
     )
     fig.write_image("out.pdf")
     fig.show()
