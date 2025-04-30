dc0800faadc8a9671ce2482e1bf7287da5255a23
Newton Ni <nwtnni@gmail.com>
2025-04-30 14:04:59 -0500

    Start chameleon setup script

---
+++ bench/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+bash <(curl -L https://nixos.org/nix/install) --no-daemon
+
+curl -sfL https://direnv.net/install.sh | sudo -E bin_path=/usr/bin bash
+
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+
+git clone git@github.com:nwtnni/cxlalloc.git
+cd cxlalloc
+git submodule update --init --recursive
